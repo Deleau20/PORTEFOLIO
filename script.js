3e7f38a87784d43966147b40b931d99df63ef702
Fred <fredkesse1234@gmail.com>
2023-10-19 17:14:34 +0000

    ajoût des effets de scroll

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
+
+  // Initialize ScrollReveal
+  ScrollReveal().reveal('.title', {
+    delay: 200,         // Délai en millisecondes avant la révélation de l'élément
+    duration: 800,      // Durée de l'animation de révélation en millisecondes
+    origin: 'bottom',   // Point d'origine de l'animation (en haut, en bas, à gauche, à droite, etc.)
+    distance: '20px',   // Distance de l'animation
+    easing: 'cubic-bezier(0.5, 0, 0, 1)', 
+  });
+
+  // Initialize ScrollReveal
+  ScrollReveal().reveal('.experience-details-container', {
+    delay: 200,         // Délai en millisecondes avant la révélation de l'élément
+    duration: 800,      // Durée de l'animation de révélation en millisecondes
+    origin: 'bottom',   // Point d'origine de l'animation (en haut, en bas, à gauche, à droite, etc.)
+    distance: '20px',   // Distance de l'animation
+    easing: 'cubic-bezier(0.5, 0, 0, 1)', 
+  });
